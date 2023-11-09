87a4758cb0b74a93b28eb520739242978008053c
Philip Buchbender <philip.yannic@gmail.com>
2023-11-09 17:30:54 +0100

    tests

--- langchain/basics/RestaurantNameGenerator/secret_key.py
+++
@@ -1,3 +0,0 @@
-openapi_key = ""
-serpapi_key = ""
-hugging_token = "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"
--- langchain/basics/secret_key.py
+++
@@ -1,3 +0,0 @@
-openapi_key = "" # remove key
-serpapi_key = "b0bf8059855d6a8c47f3ca881602a7297bed661fc2c99d411c127e56cf6b41db"
-hugging_token = "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"
